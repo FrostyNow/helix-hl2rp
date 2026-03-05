76036c4ff9ab36b24ca4df374fbc0f98d04b4fd1
Frosty <thks1018@gmail.com>
2026-03-05 12:12:04 +0900

    Fix wrong function name

--- plugins/cinematictext/sv_plugin.lua
+++ plugins/cinematictext/sv_plugin.lua
@@ -4,40 +4,38 @@ util.AddNetworkString("openCinematicSplashMenu")
 util.AddNetworkString("triggerCinematicSplashMenu")
 
 net.Receive("triggerCinematicSplashMenu", function(_, client)
-    if not client:IsAdmin() then return end
+	if not client:IsAdmin() then return end
 
-    local text = net.ReadString()
-    local bigText = net.ReadString()
-    local duration = net.ReadUInt(6)
-    local blackBars = net.ReadBool()
-    local music = net.ReadString()
-    local color = net.ReadColor()
+	local text = net.ReadString()
+	local bigText = net.ReadString()
+	local duration = net.ReadUInt(6)
+	local blackBars = net.ReadBool()
+	local music = net.ReadString()
+	local color = net.ReadColor()
 
-    net.Start("triggerCinematicSplashMenu")
-        net.WriteString(text)
-        net.WriteString(bigText)
-        net.WriteUInt(duration, 6)
-        net.WriteBool(blackBars)
-        net.WriteString(music)
-        net.WriteColor(color)
-    net.Broadcast()
+	net.Start("triggerCinematicSplashMenu")
+		net.WriteString(text)
+		net.WriteString(bigText)
+		net.WriteUInt(duration, 6)
+		net.WriteBool(blackBars)
+		net.WriteString(music)
+		net.WriteColor(color)
+	net.Broadcast()
 end)
 
-function PLUGIN:OnCharacterLoaded(character)
-    local client = character:GetPlayer()
+function PLUGIN:PlayerLoadedCharacter(client, character)
+	if (ix.config.Get("cinematicTextAuto", false)) then
+		local text = ix.config.Get("cinematicTextAutoLine1", "")
+		local bigText = ix.config.Get("cinematicTextAutoLine2", "")
+		local music = ix.config.Get("cinematicTextAutoMusic", "")
 
-    if (ix.config.Get("cinematicTextAuto", false)) then
-        local text = ix.config.Get("cinematicTextAutoLine1", "")
-        local bigText = ix.config.Get("cinematicTextAutoLine2", "")
-        local music = ix.config.Get("cinematicTextAutoMusic", "")
-
-        net.Start("triggerCinematicSplashMenu")
-            net.WriteString(text)
-            net.WriteString(bigText)
-            net.WriteUInt(6, 6) -- Default duration for auto
-            net.WriteBool(true) -- Always black bars for auto? Or maybe config? I'll stick to true.
-            net.WriteString(music)
-            net.WriteColor(color_white)
-        net.Send(client)
-    end
+		net.Start("triggerCinematicSplashMenu")
+			net.WriteString(text)
+			net.WriteString(bigText)
+			net.WriteUInt(6, 6) -- Default duration for auto
+			net.WriteBool(true) -- Always black bars for auto? Or maybe config? I'll stick to true.
+			net.WriteString(music)
+			net.WriteColor(color_white)
+		net.Send(client)
+	end
 end
