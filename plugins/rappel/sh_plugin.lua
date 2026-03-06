5401ea7f380dadd36596d3382c115d96a89cf000
MCRoNald <35054761+RonaldMacdonaldTrump@users.noreply.github.com>
2026-03-06 17:02:46 +0900

    Rappelling plugin added

--- plugins/rappel/sh_plugin.lua
+++
@@ -1,9 +0,0 @@
-local PLUGIN = PLUGIN
-
-PLUGIN.Name = "Rappel"
-PLUGIN.Description = "Allows players to rappel down buildings."
-PLUGIN.Author = "Ronald"
-
-if (SERVER) then
-    
-end
---
+++ plugins/rappelling/items/weapons/sh_rappelgear.lua
@@ -0,0 +1,8 @@
+ITEM.name = "Rappelling Gear"
+ITEM.description = "A hook used to rappel down buildings."
+ITEM.category = "Utility"
+ITEM.model = "models/gibs/metal_gib4.mdl"
+ITEM.class = "rappel_gear"
+ITEM.width = 1
+ITEM.height = 1
+
