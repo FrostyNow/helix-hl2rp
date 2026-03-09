b8399bbfc76fa3be019b327161db4bf76377e48f
MCRoNald <35054761+RonaldMacdonaldTrump@users.noreply.github.com>
2026-03-09 12:10:33 +0900

    change description

--- plugins/itemstack/sh_plugin.lua
+++ plugins/itemstack/sh_plugin.lua
@@ -2,7 +2,7 @@
 local PLUGIN = PLUGIN
 
 PLUGIN.name = "Item Stack"
-PLUGIN.author = "Custom"
+PLUGIN.author = "Ronald"
 PLUGIN.description = "Allows stackable items to share the same inventory slot while preserving individual item data."
 
 ix.util.Include("libs/sh_stack.lua", "shared")
