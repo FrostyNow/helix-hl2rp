efbc62af455be21df59efe91ab4330504576df61
Frosty <thks1018@gmail.com>
2026-03-14 10:37:45 +0900

    Fix forcefield with prop, ragdoll, and npcs

--- entities/entities/ix_forcefield.lua
+++ entities/entities/ix_forcefield.lua
@@ -15,14 +15,14 @@ ix.lang.AddTable("english", {
 	ffModeTitle = "Barrier mode changed to: %s",
 	ffModeOff = "Off",
 	ffModeCID = "Only allow citizens with valid CID",
-	ffModeNone = "Allow no citizens"
+	ffModeNone = "Only allow Combine"
 })
 
 ix.lang.AddTable("korean", {
 	ffModeTitle = "장벽 모드 변경: %s",
 	ffModeOff = "꺼짐",
 	ffModeCID = "유효한 ID 카드를 소지한 시민만 허용",
-	ffModeNone = "시민 통과 불가"
+	ffModeNone = "콤바인만 허용"
 })
 
 local MODE_ALLOW_ALL = 1
@@ -62,24 +62,6 @@ function ENT:SetupDataTables()
 end
 
 -- Helper to check if a player is authorized (Combine or has Comkey)
-function ENT:IsAuthorized(client)
-	if (!IsValid(client) or !client:IsPlayer()) then return false end
-	
-	if (client:IsCombine()) then
-		return true
-	end
-	
-	local character = client:GetCharacter()
-	if (character) then
-		local inventory = character:GetInventory()
-		if (inventory and inventory:HasItem("comkey")) then
-			return true
-		end
-	end
-	
-	return false
-end
-
 -- Helper to create physics mesh with thickness to prevent "catching"
 function ENT:CreateShieldPhysics(dummyPos)
 	local thickness = 2
@@ -406,12 +388,44 @@ local function PlayerHasCID(client)
 	return inventory and inventory:HasItem("cid") or false
 end
 
+local function PlayerHasCombineAccess(client)
+	if (!IsValid(client) or !client:IsPlayer()) then
+		return false
+	end
+
+	if (client:IsCombine()) then
+		return true
+	end
+
+	local character = client:GetCharacter()
+
+	if (!character) then
+		return false
+	end
+
+	local inventory = character:GetInventory()
+
+	return inventory and inventory:HasItem("comkey") or false
+end
+
+function ENT:IsAuthorized(client)
+	return PlayerHasCombineAccess(client)
+end
+
 local function GetRagdollOwner(ragdoll)
 	if (!IsValid(ragdoll) or !ragdoll:IsRagdoll()) then
 		return nil
 	end
 
 	local owner = ragdoll:GetNetVar("player")
+
+	if (!IsValid(owner)) then
+		owner = ragdoll.ixPlayer
+	end
+
+	if (!IsValid(owner) and ragdoll.GetNWEntity) then
+		owner = ragdoll:GetNWEntity("player")
+	end
 
 	if (IsValid(owner) and owner:IsPlayer()) then
 		return owner
@@ -434,20 +448,24 @@ local function GetVehicleDriver(vehicle)
 	return nil
 end
 
-local function ShouldCollideWithUnauthorizedPlayer(entity, mode, client)
+local function CanPlayerPass(mode, client)
 	if (!IsValid(client) or !client:IsPlayer()) then
 		return false
 	end
 
-	if (entity.IsAuthorized and entity:IsAuthorized(client)) then
-		return false
+	if (PlayerHasCombineAccess(client)) then
+		return true
 	end
 
 	if (mode == MODE_ALLOW_CID) then
-		return !client:IsCombine() and !PlayerHasCID(client)
+		return PlayerHasCID(client)
 	end
 
-	return true
+	return false
+end
+
+local function ShouldCollideWithUnauthorizedPlayer(mode, client)
+	return !CanPlayerPass(mode, client)
 end
 
 local function ShouldCollideWithNPC(mode, npc)
@@ -466,76 +484,60 @@ local function ShouldCollideWithNPC(mode, npc)
 	return true
 end
 
+local function GetForcefieldPair(a, b)
+	if (IsValid(a) and a:GetClass() == "ix_forcefield") then
+		return a, b
+	end
+
+	if (IsValid(b) and b:GetClass() == "ix_forcefield") then
+		return b, a
+	end
+
+	return nil, nil
+end
+
 -- Shared hook to allow client-side prediction and smooth passing
 hook.Add("ShouldCollide", "ix_forcefields", function(a, b)
-	local client
-	local entity
-	local ragdoll
-	local vehicle
-	local npc
+	local entity, other = GetForcefieldPair(a, b)
 
-	if (a:IsPlayer()) then
-		client = a
-		entity = b
-	elseif (b:IsPlayer()) then
-		client = b
-		entity = a
-	elseif (a:IsRagdoll()) then
-		ragdoll = a
-		entity = b
-	elseif (b:IsRagdoll()) then
-		ragdoll = b
-		entity = a
-	elseif (a:IsVehicle()) then
-		vehicle = a
-		entity = b
-	elseif (b:IsVehicle()) then
-		vehicle = b
-		entity = a
-	elseif (a:IsNPC()) then
-		npc = a
-		entity = b
-	elseif (b:IsNPC()) then
-		npc = b
-		entity = a
+	if (!IsValid(entity) or !IsValid(other)) then
+		return
 	end
 
-	if (IsValid(entity) and entity:GetClass() == "ix_forcefield") then
-		local mode = (entity.GetMode and entity:GetMode()) or 1
-		
-		-- If forcefield is OFF (Mode 1), everyone and everything passes
-		if (mode == 1) then
-			return false
-		end
+	local mode = (entity.GetMode and entity:GetMode()) or MODE_ALLOW_ALL
 
-		if (IsValid(ragdoll)) then
-			local owner = GetRagdollOwner(ragdoll)
-
-			if (!IsValid(owner)) then
-				return mode != MODE_ALLOW_CID
-			end
-
-			return ShouldCollideWithUnauthorizedPlayer(entity, mode, owner)
-		end
-
-		if (IsValid(vehicle)) then
-			local driver = GetVehicleDriver(vehicle)
-
-			if (!IsValid(driver)) then
-				return false
-			end
-
-			return ShouldCollideWithUnauthorizedPlayer(entity, mode, driver)
-		end
-
-		if (IsValid(client)) then
-			return ShouldCollideWithUnauthorizedPlayer(entity, mode, client)
-		end
-
-		if (IsValid(npc)) then
-			return ShouldCollideWithNPC(mode, npc)
-		else
-			return false
-		end
+	-- If forcefield is OFF (Mode 1), everyone and everything passes.
+	if (mode == MODE_ALLOW_ALL) then
+		return false
 	end
+
+	if (other:IsPlayer()) then
+		return ShouldCollideWithUnauthorizedPlayer(mode, other)
+	end
+
+	if (other:IsVehicle()) then
+		local driver = GetVehicleDriver(other)
+
+		if (IsValid(driver)) then
+			return ShouldCollideWithUnauthorizedPlayer(mode, driver)
+		end
+
+		return true
+	end
+
+	if (other:IsRagdoll()) then
+		local owner = GetRagdollOwner(other)
+
+		if (IsValid(owner)) then
+			return ShouldCollideWithUnauthorizedPlayer(mode, owner)
+		end
+
+		return false
+	end
+
+	if (other:IsNPC()) then
+		return ShouldCollideWithNPC(mode, other)
+	end
+
+	return false
 end)
