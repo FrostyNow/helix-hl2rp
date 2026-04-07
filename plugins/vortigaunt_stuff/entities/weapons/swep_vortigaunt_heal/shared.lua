3c5eceaa2e90623795a0424adb07fbe8a76bd9ee
Frosty <thks1018@gmail.com>
2026-04-07 16:08:33 +0900

    Vortigaunt reviving logic

--- plugins/vortigaunt_stuff/entities/weapons/swep_vortigaunt_heal/shared.lua
+++ plugins/vortigaunt_stuff/entities/weapons/swep_vortigaunt_heal/shared.lua
@@ -89,8 +89,13 @@ function SWEP:PrimaryAttack()
 	-- self.Owner:SetAnimation( PLAYER_ATTACK1 )
 
 	local eye = self.Owner:GetEyeTrace()
+	local target = eye.Entity
+	local bIsPlayer = target:IsPlayer()
+	local bIsRagdoll = target:GetClass() == "prop_ragdoll"
+	local ply = bIsRagdoll and target:GetNetVar("player")
+
+	if (!bIsPlayer and !(bIsRagdoll and IsValid(ply) and !ply:Alive())) then return end
 	
-	if (!eye.Entity:IsPlayer()) then return end
 	if self.Owner:Health() <= 50 then
 		if (SERVER) and CurTime() > (self.NextNotifyTime or 0) then
 			self.Owner:NotifyLocalized("tooWeakToHeal")
@@ -100,19 +105,18 @@ function SWEP:PrimaryAttack()
 	end
 	
 	-- Check stamina
-	if self.Owner:GetLocalVar("stm", 0) < 20 then
+	local stmCost = (bIsRagdoll and IsValid(ply) and !ply:Alive()) and 40 or 20
+	if self.Owner:GetLocalVar("stm", 0) < stmCost then
 		if (SERVER) and CurTime() > (self.NextNotifyTime or 0) then
 			self.Owner:NotifyLocalized("notEnoughStamina")
 			self.NextNotifyTime = CurTime() + 2
 		end
 		return
 	end
-	
-	local target = eye.Entity
 
 	if target:GetPos():Distance(self.Owner:GetShootPos()) > 105 then return end
 
-	if target:Health() >= target:GetMaxHealth() then
+	if bIsPlayer and target:Health() >= target:GetMaxHealth() then
 		if (SERVER) and CurTime() > (self.NextNotifyTime or 0) then
 			self.Owner:NotifyLocalized("targetFullHealth")
 			self.NextNotifyTime = CurTime() + 2
@@ -123,30 +127,88 @@ function SWEP:PrimaryAttack()
 	self:DispatchEffect("vortigaunt_charge_token")
 
 	if (SERVER) then
-
 		self.Owner:ForceSequence("heal_cycle")
-
 		self.Owner:EmitSound( "npc/vort/health_charge.wav", 100, 150, 1, CHAN_AUTO )
 		self.Owner:Freeze(true)
-
-		
 	end
-	timer.Simple(2,function() 
-		self.Owner:StopParticles()
-		if (!self.Owner:Alive()) then return end
+
+	timer.Simple(2, function() 
+		if (!IsValid(self) or !IsValid(self.Owner) or !self.Owner:Alive()) then return end
+		
 		if (SERVER) then
-			if target:GetPos():Distance(self.Owner:GetShootPos()) <= 105 then
-				local randomNum = math.random(ix.config.Get("VortHealMin", 5),ix.config.Get("VortHealMax", 20))
-				target:SetHealth(math.Clamp(target:Health()+randomNum, 0, target:GetMaxHealth()))
-				
-				-- Consume stamina
-				self.Owner:ConsumeStamina(20)
-				
-				self.Owner:StopSound("npc/vort/health_charge.wav") 
-				self.Owner:Freeze(false)
-			else
-				self.Owner:StopSound("npc/vort/health_charge.wav") 
-				self.Owner:Freeze(false)
+			self.Owner:StopSound("npc/vort/health_charge.wav") 
+			self.Owner:Freeze(false)
+			self.Owner:StopParticles()
+
+			if IsValid(target) and target:GetPos():Distance(self.Owner:GetShootPos()) <= 105 then
+				if (target:IsPlayer() and target:Alive()) then
+					local randomNum = math.random(ix.config.Get("VortHealMin", 5), ix.config.Get("VortHealMax", 20))
+					target:SetHealth(math.Clamp(target:Health() + randomNum, 0, target:GetMaxHealth()))
+					
+					self.Owner:ConsumeStamina(20)
+				elseif (bIsRagdoll and IsValid(ply) and !ply:Alive()) then
+					-- Revive logic
+					local pos = target:GetPos()
+					local angles = target:GetAngles()
+
+					target.ixIsReviving = true
+					ply.ixIsReviving = true
+					ply:Spawn()
+
+					timer.Simple(0, function()
+						if (IsValid(ply)) then
+							local revivePos = pos
+							local playerMins = ply:OBBMins()
+							local playerMaxs = ply:OBBMaxs()
+
+							local function IsSafe(checkPos)
+								local trace = {
+									start = checkPos,
+									endpos = checkPos,
+									filter = {ply, target},
+									mins = playerMins,
+									maxs = playerMaxs,
+									mask = MASK_PLAYERSOLID
+								}
+								return !util.TraceEntity(trace, ply).StartSolid
+							end
+
+							if (!IsSafe(revivePos)) then
+								local found = false
+								for i = 1, 3 do
+									local distance = i * 32
+									for j = 0, 7 do
+										local ang = j * 45
+										local rad = math.rad(ang)
+										local offset = Vector(math.cos(rad) * distance, math.sin(rad) * distance, 8)
+										local testPos = pos + offset
+
+										if (IsSafe(testPos)) then
+											revivePos = testPos
+											found = true
+											break
+										end
+									end
+									if (found) then break end
+								end
+							else
+								revivePos = revivePos + Vector(0, 0, 8)
+							end
+
+							ply:SetPos(revivePos)
+							ply:SetEyeAngles(Angle(0, angles.y, 0))
+							ply:SetHealth(25)
+
+							if (target:GetNetVar("ixRestricted")) then
+								ply:SetRestricted(true)
+							end
+						end
+					end)
+
+					self.Owner:ConsumeStamina(40) -- Revive costs more stamina
+
+					target:Remove()
+				end
 			end	
 		end
 	end)
