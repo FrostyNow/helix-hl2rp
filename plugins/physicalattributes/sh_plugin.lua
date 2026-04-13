f89c0cb5a90e1580f14cdd9d5b669eae21ff639d
Frosty <thks1018@gmail.com>
2026-04-13 19:45:51 +0900

    viewpunch and noise sway

--- plugins/physicalattributes/sh_plugin.lua
+++ plugins/physicalattributes/sh_plugin.lua
@@ -3,6 +3,18 @@ PLUGIN.name = "Attribute System"
 PLUGIN.author = "Hoooldini | Modified by Frosty"
 PLUGIN.description = "Implementation of an attribute system for roleplay."
 
+ix.lang.AddTable("english", {
+	attrViewInfo = "Attribute information for %s:",
+	cmdCharGetAttr = "View a character's attributes.",
+	cmdRollStat = "Rolls and adds a bonus for the stat provided."
+})
+
+ix.lang.AddTable("korean", {
+	attrViewInfo = "%s 캐릭터의 능력치 정보:",
+	cmdCharGetAttr = "대상 캐릭터의 능력치를 확인합니다.",
+	cmdRollStat = "제공된 능력치의 보너스를 더해 주사위를 굴립니다."
+})
+
 -- [[ CONFIGURATION OPTIONS ]] --
 
 ix.config.Add("enableStamina", true, "Whether or not stamina drain is enabled.", nil, {
@@ -68,6 +80,26 @@ end, {
 	category = "characters"
 })
 
+ix.config.Add("gunAimPunch", 1, "The multiplier for aim punch when hit by a gun.", nil, {
+	data = {min = 0, max = 5, decimals = 1},
+	category = "Attributes"
+})
+
+ix.config.Add("meleeAimPunch", 1, "The multiplier for aim punch when hit by a melee weapon.", nil, {
+	data = {min = 0, max = 5, decimals = 1},
+	category = "Attributes"
+})
+
+ix.config.Add("maxAimPunch", 30, "The maximum degrees a player's aim can be punched upwards.", nil, {
+	data = {min = 10, max = 90},
+	category = "Attributes"
+})
+
+ix.config.Add("aimSwayIntensity", 1, "The intensity of the noise-based aim sway.", nil, {
+	data = {min = 0, max = 5, decimals = 1},
+	category = "Attributes"
+})
+
 -- [[ COMMANDS ]] --
 
 --[[
@@ -78,7 +110,7 @@ end, {
 
 ix.command.Add("RollStat", {
 	syntax = "<stat>",
-	description = "Rolls and adds a bonus for the stat provided.",
+	description = "@cmdRollStat",
 	arguments = {
 		ix.type.text
 	},
@@ -93,6 +125,26 @@ ix.command.Add("RollStat", {
 				max = maximum
 			})
 		end
+	end
+})
+
+ix.command.Add("CharGetAttr", {
+	syntax = "<string target>",
+	description = "@cmdCharGetAttr",
+	adminOnly = true,
+	arguments = {
+		ix.type.character
+	},
+	OnRun = function(self, client, target)
+		local text = "\n" .. L("attrViewInfo", client, target:GetName())
+		
+		for k, v in pairs(ix.attributes.list) do
+			local value = target:GetAttribute(k, 0)
+			local name = L(v.name or k, client)
+			text = text .. string.format("\n- %s (%s): %s", name, k, value)
+		end
+
+		return text
 	end
 })
 
@@ -228,23 +280,82 @@ if (SERVER) then
 	end
 
 	function PLUGIN:EntityTakeDamage(entity, dmgInfo)
-		if (!IsValid(entity) or !entity:IsPlayer()) then
+		if (!IsValid(entity) or !entity:IsPlayer() or dmgInfo:GetDamage() <= 0) then
 			return
 		end
 
-		local inflictor = dmgInfo:GetInflictor()
+		local bIsBullet = dmgInfo:IsBulletDamage()
+		local bIsMelee = dmgInfo:IsDamageType(DMG_CLUB) or dmgInfo:IsDamageType(DMG_SLASH) or dmgInfo:IsDamageType(DMG_GENERIC) or dmgInfo:IsDamageType(DMG_SHOCK)
+		local bIsBlast = dmgInfo:IsDamageType(DMG_BLAST)
+		local bIsFall = dmgInfo:IsDamageType(DMG_FALL)
 
-		if (!IsValid(inflictor)) then
-			return
+		local armorMultiplier = (entity:Armor() > 0) and 0.5 or 1
+		local endurance = 0
+		local maxAttr = ix.config.Get("maxAttributes", 30)
+		local character = entity:GetCharacter()
+
+		if (character) then
+			endurance = character:GetAttribute("end", 0)
 		end
 
-		-- if (inflictor:GetClass() == "ix_hands") then
-		-- 	dmgInfo:SetDamage(math.Clamp(dmgInfo:GetDamage(), 1, 10))
-		-- elseif (inflictor:GetClass() == "ix_stunstick") then
-		-- 	local maxDamage = (inflictor.GetActivated and inflictor:GetActivated()) and 20 or 15
-		-- 	local minDamage = (inflictor.GetActivated and inflictor:GetActivated()) and 3 or 1
-		-- 	dmgInfo:SetDamage(math.Clamp(dmgInfo:GetDamage(), minDamage, maxDamage))
-		-- end
+		-- Reduce punch intensity based on endurance (up to 85% reduction at max endurance)
+		local attribMultiplier = 1 - (math.min(endurance, maxAttr) / maxAttr * 0.85)
+		local finalMultiplier = armorMultiplier * attribMultiplier
+
+		-- Accumulate and limit aim punch to prevent "lifting to the sky"
+		local curTime = CurTime()
+		entity.ixAimPunchAccum = entity.ixAimPunchAccum or 0
+		entity.ixLastAimPunch = entity.ixLastAimPunch or 0
+
+		-- Decay accumulated punch (roughly 20 degrees per second)
+		entity.ixAimPunchAccum = math.max(0, entity.ixAimPunchAccum - (curTime - entity.ixLastAimPunch) * 20)
+		entity.ixLastAimPunch = curTime
+
+		local maxPunch = ix.config.Get("maxAimPunch", 30)
+		local punchUp = 0
+		local punchSide = math.Rand(-0.8, 0.8)
+
+		-- Apply aim punch effect
+		if (bIsBullet) then
+			local multiplier = ix.config.Get("gunAimPunch", 1) * finalMultiplier
+
+			if (multiplier > 0) then
+				punchUp = math.Rand(1.5, 3.5) * multiplier
+				punchSide = punchSide * multiplier
+			end
+		elseif (bIsMelee) then
+			local multiplier = ix.config.Get("meleeAimPunch", 1) * finalMultiplier
+
+			if (multiplier > 0) then
+				punchUp = math.Rand(1.0, 2.5) * multiplier
+				punchSide = punchSide * multiplier
+			end
+		elseif (bIsBlast) then
+			local multiplier = ix.config.Get("gunAimPunch", 1) * finalMultiplier
+
+			if (multiplier > 0) then
+				punchUp = math.Rand(6.0, 10.0) * multiplier
+				punchSide = math.Rand(-4.0, 4.0) * multiplier
+			end
+		elseif (bIsFall) then
+			-- Armor doesn't usually protect against fall impact, so we only use endurance
+			local multiplier = ix.config.Get("meleeAimPunch", 1) * attribMultiplier
+
+			if (multiplier > 0) then
+				punchUp = math.Rand(3.0, 6.0) * multiplier
+				punchSide = math.Rand(-1.5, 1.5) * multiplier
+			end
+		end
+
+		-- Cap the punch if it exceeds the max
+		if (entity.ixAimPunchAccum + punchUp > maxPunch) then
+			punchUp = math.max(0, maxPunch - entity.ixAimPunchAccum)
+		end
+
+		if (punchUp > 0) then
+			entity:ViewPunch(Angle(-punchUp, punchSide, 0))
+			entity.ixAimPunchAccum = entity.ixAimPunchAccum + punchUp
+		end
 
 		-- Stunstick melee scaling is handled in plugins/wepadjust.lua so the
 		-- attacker's strength is applied once, consistently with other melee weapons.
@@ -275,5 +386,69 @@ if (SERVER) then
 		if (client:GetCharacter() and IsValid(trace.Entity) and trace.Entity:IsPlayer()) then
 			client:GetCharacter():UpdateAttrib("str", 0.001)
 		end
+	end
+end
+
+if (CLIENT) then
+	local swayX, swayY = 0, 0
+	local weaponWhitelist = {
+		-- ["ix_hands"] = true,
+		["ix_keys"] = true,
+		["ix_suitcase"] = true,
+		["swep_vortigaunt_sweep"] = true,
+		["gmod_tool"] = true,
+		["gmod_physgun"] = true,
+		["weapon_physgun"] = true,
+		["weapon_physcannon"] = true
+	}
+
+	function PLUGIN:CreateMove(cmd)
+		local client = LocalPlayer()
+		if (!IsValid(client) or !client:Alive() or !client:GetCharacter()) then return end
+		
+		-- Only apply when weapon is raised
+		if (!client:IsWeaponRaised()) then return end
+
+		local weapon = client:GetActiveWeapon()
+		if (!IsValid(weapon)) then return end
+
+		-- Whitelist check
+		local class = weapon:GetClass():lower()
+		if (weaponWhitelist[class] or class:find("tool") or class:find("physgun") or class:find("scanner")) then
+			return
+		end
+
+		local endurance = client:GetCharacter():GetAttribute("end", 0)
+		local maxAttr = ix.config.Get("maxAttributes", 30)
+		
+		-- Higher endurance leads to much lower sway
+		local baseIntensity = ix.config.Get("aimSwayIntensity", 1) * math.max(0.02, 1 - (endurance / maxAttr)) * 0.12
+
+		-- Hybrid Approach: Intensity increases based on movement speed
+		local velocity = client:GetVelocity():Length2D()
+		-- We use a normalized velocity to increase sway up to 3.5x when running
+		local moveMultiplier = 1 + (velocity / math.max(ix.config.Get("runSpeed", 225), 1)) * 2.5
+		
+		-- Vertical movement (Jumping/Falling)
+		if (!client:IsOnGround()) then
+			-- Significantly increase sway when in the air, scaled by vertical velocity
+			local verticalVel = math.abs(client:GetVelocity().z)
+			moveMultiplier = moveMultiplier + 3 + (verticalVel / 500) * 5
+		end
+
+		local intensity = baseIntensity * moveMultiplier
+
+		if (intensity <= 0) then return end
+
+		local time = CurTime()
+		-- Multi-frequency sine waves to simulate semi-random noise
+		swayX = (math.sin(time * 0.5) + math.sin(time * 1.2) * 0.5 + math.sin(time * 2.5) * 0.2)
+		swayY = (math.cos(time * 0.4) + math.cos(time * 1.4) * 0.5 + math.cos(time * 2.2) * 0.2)
+		
+		local angles = cmd:GetViewAngles()
+		angles.p = angles.p + swayY * intensity
+		angles.y = angles.y + swayX * intensity
+		
+		cmd:SetViewAngles(angles)
 	end
 end
